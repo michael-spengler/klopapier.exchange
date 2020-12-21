7d6c6db1a2c9fa35358060dd0f2e5ea0bb360ddb
Troy Kessler <troy.kessler99@gmail.com>
2020-12-21 11:30:18 +0100

    added tailwindcss

---
+++ src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <HelloWorld msg="Klopapier Exchange"/>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default defineComponent({
+  components: {
+    HelloWorld
+  }
+})
+</script>
