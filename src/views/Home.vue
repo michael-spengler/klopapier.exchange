d2e73517ad958664b0a402f5a4200370eff12a9b
Michael <michael.spengler@sap.com>
2020-12-22 00:44:33 +0100

    add the bird again and update some texts

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -18,65 +18,35 @@
           type="button"
           class="inline-flex items-center px-4 py-2 mx-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
         >
-          <span class="material-icons mr-2">
-            show_chart
-          </span>
+          <span class="material-icons mr-2"> show_chart </span>
           Play
         </button>
+
         <button
           type="button"
           class="inline-flex items-center px-4 py-2 mx-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
         >
-          <span class="material-icons mr-2">
-            psychology
-          </span>
+          <span class="material-icons mr-2"> psychology </span>
           Learn
         </button>
+
+        <video width="320" height="240" controls>
+          <source src=”./../assets/geiler-vogel.mp4” type=video/ogg> <source
+          src="./../assets/geiler-vogel.mp4" type=video/mp4>
+        </video>
       </div>
     </div>
+
     <div id="particles-js"></div>
     <div class="mt-12 grid grid-cols-3 gap-12">
       <div class="shadow p-4 rounded">
-        <div class="font-bold">Our Currencies</div>
-        We have created two currencies on the Ethereum Blockchain for this.
-        There is and will be a fixed amout of 100
-        <a
-          class="text-blue-600"
-          href="https://etherscan.io/token/0xe79ff9299351356e0ff64fd11ca594162cbd189a"
-          target="_blank"
-          rel="noopener"
-          >USDWipePaper (UWP)</a
-        >
-        and also a fixed amount of 100
-        <a
-          class="text-blue-600"
-          href="https://etherscan.io/token/0xcfe63389382412c31d4dfcf60a70fa5a9b90b63f"
-          target="_blank"
-          rel="noopener"
-          >EURWipePaper (EWP)</a
-        >
-        available.
+        <div class="font-bold">The Idea</div>
+
+        The idea is to provide a money exploration game. By playing this game you can learn more about money than they teach you at most universities.
       </div>
-      <div class="shadow p-4 rounded">
-        <div class="font-bold">Wipepapers</div>
-        <div>
-          You will be able to purchase Central Bank Wipepaper (CBWP =?
-          <a
-            class="text-blue-600"
-            href="https://www.youtube.com/watch?v=jWM-MdT2rDg&t=547s"
-            target="_blank"
-            rel="noopener"
-            >CBDC</a
-          >) while paying with money which is from the people for the people -
-          aka crypto money.
-        </div>
-        <div>
-          With this we want to make your exploration regarding crypto currencies
-          fun and valuable at the same time.
-        </div>
-      </div>
-      <div class="shadow p-4 rounded">
-        <div class="font-bold">Additional Information</div>
+
+            <div class="shadow p-4 rounded">
+        <div class="font-bold">Prerequisites</div>
         <div>
           First please explore the difference between the
           <a
@@ -108,33 +78,46 @@
           >.
         </div>
       </div>
+
+      <div class="shadow p-4 rounded">
+        <div class="font-bold">The Game</div>
+        <div>
+          You can bet Central Bank Wipepaper (CBWP =?
+          <a
+            class="text-blue-600"
+            href="https://www.youtube.com/watch?v=jWM-MdT2rDg&t=547s"
+            target="_blank"
+            rel="noopener"
+            >CBDC</a
+          >) on rising real money prices.
+        </div>
+        <div>
+          With this we want to make your exploration regarding crypto currencies fun and
+          valuable at the same time.
+        </div>
+      </div>
     </div>
     <div class="mt-32">
       <div class="grid grid-cols-2 grid-gap-64">
         <div>
-          <img
-            class="w-2/3 block mx-auto"
-            src="./../assets/history.svg"
-            alt="History"
-          />
+          <img class="w-2/3 block mx-auto" src="./../assets/history.svg" alt="History" />
         </div>
         <div>
           <div class="font-bold text-xl">History</div>
           <div>
-            For several decades wirepullers behind most central banks could
-            create their money out of thin air. The incentives for giving this
-            money to people who create the most value for other people - like
-            farmers, nurses, doctors and many more - have not been strong enough
-            to foster a fair system.
+            For several decades wirepullers behind most central banks could create their
+            money out of thin air. The incentives for giving this money to people who
+            create the most value for other people - like farmers, nurses, doctors and
+            many more - have not been strong enough to foster a fair system.
           </div>
           <div>
-            In order to foster fair systems we invested some time into
-            professional incentive engineering. We came to the conclusion that
-            Ether, Bitcoin & Co. can play valuable roles in this game.
+            In order to foster fair systems we invested some time into professional
+            incentive engineering. We came to the conclusion that Ether, Bitcoin & Co. can
+            play valuable roles in this game.
           </div>
           <div>
-            It seems strange that about 95 % (rough guess) of adult people on
-            earth don't even have a
+            It seems strange that about 95 % (rough guess) of adult people on earth don't
+            even have a
             <a
               class="text-blue-500"
               href="https://brave.com"
@@ -152,8 +135,7 @@
         <div>
           <div class="font-bold text-xl">Contribute</div>
           <div>
-            This project is mainly an education playground for some of my
-            students at
+            This project is mainly an education playground for some of my students at
             <a
               class="text-blue-500"
               href="https://www.mannheim.dhbw.de"
@@ -163,16 +145,14 @@
             >.
           </div>
           <div>
-            At the same time we are happy about high quality contributions from
-            anyone.
+            At the same time we are happy about high quality contributions from anyone.
           </div>
           <div>
-            We want to provide valuable and fun education around decentralized
-            incentive engineering and peer to peer collaboration.
+            We want to provide valuable and fun education around decentralized incentive
+            engineering and peer to peer collaboration.
           </div>
           <div>
-            Along this way we also promote cool platforms in the DeFi ecosystem
-            like
+            Along this way we also promote cool platforms in the DeFi ecosystem like
             <a
               class="text-blue-500"
               href="https://gitcoin.co/"
@@ -212,8 +192,8 @@
             >
           </div>
           <div>
-            If you are completely new to the crypto- and DeFi space, you might
-            also check some videos
+            If you are completely new to the crypto- and DeFi space, you might also check
+            some videos
             <a
               class="text-blue-500"
               href="https://www.youtube.com/watch?v=J8V0GkqUKDE&list=PLEz_1ekJPuAii8of1qHLxB0lxpC6QXXdY"
