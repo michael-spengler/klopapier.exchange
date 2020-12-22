cda963960bd35690e4eea8e04cc3933eedef9f38
Michael <michael.spengler@sap.com>
2020-12-22 20:01:00 +0100

    add scenario descriptions

--- acceptance-tests/landingpage_test.js
+++ acceptance-tests/landingpage_test.js
@@ -1,11 +1,38 @@
 Feature('landingpage');
 
-Scenario('test something', ({ I }) => {
+Scenario('test landingpage', ({ I }) => {
 
     I.amOnPage('https://klopapier.exchange')
     I.wait(4)
     I.see('Klopapier Exchange')
-    I.click('Play')
-    I.wait(4)
-    I.see('In our game people who work at a central bank are paid for printing Central Bank Wipepaper (CBWP) so you will first need to give them some Ether to print a bunch of CBWP for you.')
+
 });
+
+Scenario('test connect browserwallet', ({ I }) => { // John
+
+    I.amOnPage('https://klopapier.exchange')
+    I.wait(4)
+    I.connectBrowserWallet()
+
+});
+
+Scenario('test user swapping from ETH to CBWP', ({ I }) => { // Fabian
+
+    I.amOnPage('https://klopapier.exchange')
+    I.wait(4)
+    I.swapFromETHToCBWP(0.002)
+
+});
+
+
+Scenario('test user swapping from CBWP to ETH', ({ I }) => { // Fabian
+    // youtube video ... 
+
+    // backing with ETH (e.g. 1 ETH === 100 % of CBWP)
+
+    I.amOnPage('https://klopapier.exchange')
+    I.wait(4)
+    I.swapCBWPToETH('0.002')
+
+});
+
