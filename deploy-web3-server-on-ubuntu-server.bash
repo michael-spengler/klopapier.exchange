4121b4db6b5399829c96a5c3b0bdeef25e21b258
Michael <michael.spengler@sap.com>
2020-12-24 20:16:05 +0100

    apply boy scout rule

--- deploy-web3-server-on-ubuntu-server.bash
+++ deploy-web3-server-on-ubuntu-server.bash
@@ -9,6 +9,6 @@ npm config set ignore-scripts false
 cd /root/klopapier.exchange/web3-proxy-nodejs-infura && tsc
 # if you start it the first time try things out via
 # ts-node src/web3-server.ts or for the long run
-# pm2 start dist/web3-server.js
+# pm2 start /root/klopapier.exchange/web3-proxy-nodejs-infura/dist/web3-server.js
 # pm2 start 
 pm2 restart 1 
