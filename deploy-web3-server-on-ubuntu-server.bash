e5bfd10fe7e1916a4d3b2a61d5e21bef07a3a7e0
Michael <michael.spengler@sap.com>
2020-12-24 19:55:32 +0100

    add web3 server deployment related info

--- deploy-web3-server-on-ubuntu-server.bash
+++ deploy-web3-server-on-ubuntu-server.bash
@@ -6,4 +6,9 @@ npm config set ignore-scripts true
 cd /root/klopapier.exchange/web3-proxy-nodejs-infura && npm i
 npm config set ignore-scripts false
 
-pm2 restart 1 # or (if first time or so) ts-node web3-server.ts 
+tsc
+# if you start it the first time try things out via
+# ts-node web3-server.ts or for the long run
+pm2 start dist/web3-server.ts
+# pm2 start 
+pm2 restart 1 
