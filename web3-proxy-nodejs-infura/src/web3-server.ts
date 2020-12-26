e87d6c2e47f58f3c3693b4e79eb396f0b70abca6
Michael <michael.spengler@sap.com>
2020-12-26 21:48:50 +0100

    refactor

--- web3-proxy-nodejs-infura/src/web3-server.ts
+++ web3-proxy-nodejs-infura/src/web3-server.ts
@@ -1,25 +1,24 @@
+
 import { UniSwapService } from './uniswap-service';
 import { Web3Service } from './web3-service'
-const fs = require('fs-sync')
 
+const fs = require('fs-sync')
 const http = require('http');
 const https = require('https');
+const express = require('express');
+const app = express();
 
+let expressServer
 let useHTTPS = true
 let credentials
 
 try {
     const cert = fs.read('/etc/letsencrypt/live/openforce.de/fullchain.pem', 'utf8');
-    const key  = fs.read('/etc/letsencrypt/live/openforce.de/privkey.pem', 'utf8');
-    credentials = {cert, key};
-} catch(error) {
+    const key = fs.read('/etc/letsencrypt/live/openforce.de/privkey.pem', 'utf8');
+    credentials = { cert, key };
+} catch (error) {
     useHTTPS = false
 }
-    
-const express = require('express');
-const app = express();
-
-let expressServer
 
 const infuraProjectId = fs.read(`${__dirname}/../.env`).split('=')[1]
 
@@ -27,51 +26,14 @@ const web3Service = new Web3Service(infuraProjectId)
 const uniswapService = new UniSwapService()
 
 app.get('/', (req, res) => {
-    res.send('hello world')
+    res.send('hello blockchain world - we love distributed ledgers')
 })
 
-// http://localhost:3001/getBalance/walletAddress/0x1eB9426F329d46f5Ee2263C030d6E9661f2ca694
-// https://openforce.de/getBalance/walletAddress/0x1eB9426F329d46f5Ee2263C030d6E9661f2ca694
-app.get('/getBalance/walletAddress/:account', async (req, res) => {
-    res.send(await web3Service.getBalanceInEther(req.params.account))
-})
+defineStandardRoutesWeb3JS(app)
 
-// http://localhost:3001/getGasPriceInEther
-// https://openforce.de/getGasPriceInEther
-app.get('/getGasPriceInEther', async (req, res) => {
-    res.send(await web3Service.getGasPriceInEther())
-})
+defineKlopapierSpecificRoutesWeb3JS(app)
 
-// http://localhost:3001/getERC20Balance/walletAddress/0x4396A292512AA418087645B56a3a76333Bd10e28/smartContractAddress/0xE5127cF21fb96A6241067Aa43E242a8D056bD729
-// https://openforce.de/getERC20Balance/walletAddress/0x4396A292512AA418087645B56a3a76333Bd10e28/smartContractAddress/0xE5127cF21fb96A6241067Aa43E242a8D056bD729
-app.get('/getERC20Balance/walletAddress/:walletAddress/smartContractAddress/:smartContractAddress', async (req, res) => {
-    res.send(await web3Service.getERC20Balance(req.params.walletAddress, req.params.smartContractAddress))
-})
-
-// http://localhost:3001/getTokenViaUniswap/smartContractAddress/0xE5127cF21fb96A6241067Aa43E242a8D056bD729
-// https://openforce.de/getTokenViaUniswap/smartContractAddress/0xE5127cF21fb96A6241067Aa43E242a8D056bD729
-app.get('/getTokenViaUniswap/smartContractAddress/:smartContractAddress', async (req, res) => {
-    res.send(await uniswapService.getToken(req.params.smartContractAddress))
-})
-
-// http://localhost:3001/getPairViaUniswap/smartContractAddress/0xE5127cF21fb96A6241067Aa43E242a8D056bD729
-// https://openforce.de/getPairViaUniswap/smartContractAddress/0xE5127cF21fb96A6241067Aa43E242a8D056bD729
-app.get('/getPairViaUniswap/smartContractAddress/:smartContractAddress', async (req, res) => {
-    res.send(await uniswapService.getPairViaUniswap(req.params.smartContractAddress))
-})
-
-
-// the following APIs are contract-specific and shall be moved to a dedicated express app one day
-app.post('/buyWipePaper/amount/:amount', async (req, res) => {
-    await web3Service.buyWipepaper(req.parameters.amount)
-    res.send('to be implemented')
-})
-
-app.post('/sellWipePaper/amount/:amount', async (req, res) => {
-    await web3Service.sellWipepaper(req.parameters.amount)
-    res.send('to be implemented')
-})
-
+defineStandardRoutesUniswap(app)
 
 if (useHTTPS) {
     expressServer = https.createServer(credentials, app);
@@ -83,3 +45,64 @@ if (useHTTPS) {
     expressServer.listen(3001);
 }
 
+
+
+
+
+
+
+
+/************************************************ Details ************************************************/
+
+
+function defineStandardRoutesWeb3JS(app) {
+    // http://localhost:3001/getBalance/walletAddress/0x1eB9426F329d46f5Ee2263C030d6E9661f2ca694
+    // https://openforce.de/getBalance/walletAddress/0x1eB9426F329d46f5Ee2263C030d6E9661f2ca694
+    app.get('/getBalance/walletAddress/:account', async (req, res) => {
+        res.send(await web3Service.getBalanceInEther(req.params.account))
+    })
+
+    // http://localhost:3001/getGasPriceInEther
+    // https://openforce.de/getGasPriceInEther
+    app.get('/getGasPriceInEther', async (req, res) => {
+        res.send(await web3Service.getGasPriceInEther())
+    })
+
+    // http://localhost:3001/getERC20Balance/walletAddress/0x4396A292512AA418087645B56a3a76333Bd10e28/smartContractAddress/0xE5127cF21fb96A6241067Aa43E242a8D056bD729
+    // https://openforce.de/getERC20Balance/walletAddress/0x4396A292512AA418087645B56a3a76333Bd10e28/smartContractAddress/0xE5127cF21fb96A6241067Aa43E242a8D056bD729
+    app.get('/getERC20Balance/walletAddress/:walletAddress/smartContractAddress/:smartContractAddress', async (req, res) => {
+        res.send(await web3Service.getERC20Balance(req.params.walletAddress, req.params.smartContractAddress))
+    })
+}
+
+function defineKlopapierSpecificRoutesWeb3JS(app) {
+
+    // the following APIs are contract-specific and shall be moved to a dedicated express app one day
+    app.post('/buyWipePaper/amount/:amount', async (req, res) => {
+        await web3Service.buyWipepaper(req.parameters.amount)
+        res.send('to be implemented')
+    })
+
+    app.post('/sellWipePaper/amount/:amount', async (req, res) => {
+        await web3Service.sellWipepaper(req.parameters.amount)
+        res.send('to be implemented')
+    })
+
+
+}
+function defineStandardRoutesUniswap(app) {
+
+    // http://localhost:3001/getTokenViaUniswap/smartContractAddress/0xE5127cF21fb96A6241067Aa43E242a8D056bD729
+    // https://openforce.de/getTokenViaUniswap/smartContractAddress/0xE5127cF21fb96A6241067Aa43E242a8D056bD729
+    app.get('/getTokenViaUniswap/smartContractAddress/:smartContractAddress', async (req, res) => {
+        res.send(await uniswapService.getToken(req.params.smartContractAddress))
+    })
+
+    // http://localhost:3001/getPairViaUniswap/smartContractAddress/0xE5127cF21fb96A6241067Aa43E242a8D056bD729
+    // https://openforce.de/getPairViaUniswap/smartContractAddress/0xE5127cF21fb96A6241067Aa43E242a8D056bD729
+    app.get('/getPairViaUniswap/smartContractAddress/:smartContractAddress', async (req, res) => {
+        res.send(await uniswapService.getPairViaUniswap(req.params.smartContractAddress))
+    })
+
+
+}
