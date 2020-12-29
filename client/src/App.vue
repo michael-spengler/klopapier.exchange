797fe1899a3f707a800a7410f55861306241eefc
Troy Kessler <troy.kessler99@gmail.com>
2020-12-29 20:42:53 +0100

    refactored wallet adress & account balance

--- client/src/App.vue
+++ client/src/App.vue
@@ -46,31 +46,11 @@
           </div>
           <div class="hidden md:block">
             <div class="ml-4 flex items-center md:ml-6">
-              <button
-                v-if="walletAdress"
-                class="bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
-              >
-                <span class="sr-only">View notifications</span>
-                <svg
-                  class="h-6 w-6"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  aria-hidden="true"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
-                  />
-                </svg>
-              </button>
               <div class="ml-3 relative">
                 <div>
                   <div v-if="walletAdress" class="ml-2 font-bold text-white flex items-center">
-                    {{ walletAdress }} | Ether Balance: {{ etherBalance }}
+                    <div class="bg-gray-900 text-white px-3 py-2 rounded-md">{{ walletAdressDisplay }}</div>
+                    <div class="ml-2 bg-gray-900 text-white px-3 py-2 rounded-md">{{ etherBalance }} ETH</div>
                   </div>
                   <button
                     v-else
@@ -226,13 +206,21 @@
 </template>
 
 <script>
-import { defineComponent, onMounted, ref } from "vue";
+import { computed, defineComponent, onMounted, ref } from "vue";
 import axios from 'axios';
 
 export default defineComponent({
   setup() {
     const walletAdress = ref(localStorage.getItem('klopapier.exchange.account.walletAdress'));
     const etherBalance = ref(localStorage.getItem('klopapier.exchange.account.etherBalance') || '0');
+
+    const walletAdressDisplay = computed(() => {
+      if (walletAdress.value) {
+        const adress = walletAdress.value;
+        return `${adress.substring(0, 4)}...${adress.substring(adress.length - 4, adress.length)}`
+      }
+      return ''
+    })
 
     onMounted(() => {
       window.ethereum.on('accountsChanged', (accounts) => {
@@ -271,7 +259,8 @@ export default defineComponent({
     return {
       connect,
       walletAdress,
-      etherBalance
+      etherBalance,
+      walletAdressDisplay
     };
   },
 });
--- client/src/views/Whipepaper.vue
+++ client/src/views/Whipepaper.vue
@@ -1,4 +1,5 @@
 <template>
+  <div>
     <div class="mt-4 text-xl">
       This is an education project
       <a
@@ -10,28 +11,26 @@
         under construction</a
       >.
     </div>
-
-  <p><br></p>
-  <div>
-    Perhaps the best thing you can do with Central Bank Wipe Paper (CBWP) is to invest it in betting on rising Ether and Bitcoin prices.
-
     <p><br></p>
+    <div>
+      Perhaps the best thing you can do with Central Bank Wipe Paper (CBWP) is to invest it in betting on rising Ether and Bitcoin prices.
 
-    To start betting you need to pay the central bank employees to print some CBWP for you to get the party started. How much Wipepaper do you want to be printed?
-    
-  </div>
-  
-  <p><br></p>
-  <div>
-    <vue-slider v-if="etherBalance" v-model="amount" :min="0" :max="etherBalance" :interval="0.000000000000000001" />
-    <div v-else>
-      Kein Guthaben
+      <p><br></p>
+
+      To start betting you need to pay the central bank employees to print some CBWP for you to get the party started. How much Wipepaper do you want to be printed?
     </div>
-    <p> <br>
-    </p>
-    <button @click="onBuyNowClicked()" type="button" class="ml-5 bg-white py-2 px-3 border border-gray-300 rounded-md shadow-sm text-sm leading-4 font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-      Buy now
-    </button>
+    <p><br></p>
+    <div>
+      <vue-slider v-if="etherBalance" v-model="amount" :min="0" :max="etherBalance" :interval="0.000000000000000001" />
+      <div v-else>
+        Kein Guthaben
+      </div>
+      <p> <br>
+      </p>
+      <button @click="onBuyNowClicked()" type="button" class="ml-5 bg-white py-2 px-3 border border-gray-300 rounded-md shadow-sm text-sm leading-4 font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+        Buy now
+      </button>
+    </div>
   </div>
 </template>
 
