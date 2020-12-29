ca793acdc14d7bff7d51cf5535e5901ef43f2958
Michael <michael.spengler@sap.com>
2020-12-29 18:10:21 +0100

    optimize project structure

--- deploy-web3-server-on-ubuntu-server.bash
+++ deploy-web3-server-on-ubuntu-server.bash
@@ -3,10 +3,10 @@ cd /root/klopapier.exchange
 git pull 
 
 npm config set ignore-scripts true
-cd /root/klopapier.exchange/web3-proxy-nodejs-infura && npm i
+cd /root/klopapier.exchange/server/web3-proxy-nodejs-infura && npm i
 npm config set ignore-scripts false
 
-cd /root/klopapier.exchange/web3-proxy-nodejs-infura && tsc
+cd /root/klopapier.exchange/server/web3-proxy-nodejs-infura && tsc
 # if you start it the first time try things out via
 # ts-node src/web3-server.ts or for the long run
 # pm2 start /root/klopapier.exchange/web3-proxy-nodejs-infura/dist/web3-server.js
