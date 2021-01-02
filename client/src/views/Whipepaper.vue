b01be43b41fee5a60677f720cd5976f12fb8d2a9
Michael <michael.spengler@sap.com>
2021-01-02 20:25:29 +0100

    quick fix

--- client/src/views/Whipepaper.vue
+++ client/src/views/Whipepaper.vue
@@ -138,11 +138,11 @@
             </div>
           </div>
 
-          <button
+          <!-- <button
             @click="buy"
             class="mt-12 bg-white py-2 px-3 border border-gray-300 rounded-md shadow-sm text-sm leading-4 font-bold text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-600"
           >
-          </button>
+          </button> -->
           
           <button @click="openConfirmationDialog" class="mt-12 bg-white py-2 px-3 border border-gray-300 rounded-md shadow-sm text-sm leading-4 font-bold text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-600">
             Buy now
