9574d4660400a62bf2c191d61aa81b749f07fe51
Troy Kessler <troy.kessler99@gmail.com>
2021-01-04 20:12:34 +0100

    meeting about next steps in betting game -> development of realtime data chart // 1h

--- client/src/components/EthereumChart.vue
+++ client/src/components/EthereumChart.vue
@@ -2,13 +2,19 @@
   <div>
     <apexchart type="area" :options="chartOptions" :series="chartSeries"></apexchart>
     <div class="mt-4 grid grid-cols-3 gap-8">
-      <button class="bg-white py-2 px-3 border border-gray-300 rounded-md shadow-sm text-sm leading-4 font-bold text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-600">
+      <button
+        class="bg-white py-2 px-3 border border-gray-300 rounded-md shadow-sm text-sm leading-4 font-bold text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-600"
+      >
         1 Year
       </button>
-      <button class="bg-white py-2 px-3 border border-gray-300 rounded-md shadow-sm text-sm leading-4 font-bold text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-600">
+      <button
+        class="bg-white py-2 px-3 border border-gray-300 rounded-md shadow-sm text-sm leading-4 font-bold text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-600"
+      >
         3 Years
       </button>
-      <button class="bg-white py-2 px-3 border border-gray-300 rounded-md shadow-sm text-sm leading-4 font-bold text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-600">
+      <button
+        class="bg-white py-2 px-3 border border-gray-300 rounded-md shadow-sm text-sm leading-4 font-bold text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-600"
+      >
         All
       </button>
     </div>
@@ -20,111 +26,114 @@ import { defineComponent } from "vue";
 
 const chartOptions = {
   chart: {
-    type: 'area',
+    type: "area",
     stacked: false,
     zoom: {
-      type: 'x',
+      type: "x",
       enabled: true,
-      autoScaleYaxis: true
+      autoScaleYaxis: true,
     },
     toolbar: {
-      autoSelected: 'zoom'
-    }
+      autoSelected: "zoom",
+    },
   },
   dataLabels: {
-    enabled: false
+    enabled: false,
   },
   markers: {
     size: 0,
   },
   title: {
-    text: 'Ether Price Movement',
-    align: 'left'
+    text: "Ether Price Movement",
+    align: "left",
   },
   fill: {
-    type: 'gradient',
+    type: "gradient",
     gradient: {
       shadeIntensity: 1,
       inverseColors: false,
       opacityFrom: 0.5,
       opacityTo: 0,
-      stops: [0, 90, 100]
+      stops: [0, 90, 100],
     },
   },
   yaxis: {
     title: {
-      text: 'Price in USD'
+      text: "Price in USD",
     },
   },
   xaxis: {
-    type: 'datetime',
+    type: "datetime",
   },
   tooltip: {
     shared: false,
-  }
-}
+  },
+};
 
-const chartSeries = [{
-  name: 'Ethereum',
-  data: [
-    {
-      x: new Date('2018-02-12').getTime(),
-      y: 76
-    },
-    {
-      x: new Date('2018-02-13').getTime(),
-      y: 78
-    },
-    {
-      x: new Date('2018-02-14').getTime(),
-      y: 79
-    },
-    {
-      x: new Date('2018-02-15').getTime(),
-      y: 74
-    },
-    {
-      x: new Date('2018-02-16').getTime(),
-      y: 76
-    },
-    {
-      x: new Date('2018-02-17').getTime(),
-      y: 78
-    },
-    {
-      x: new Date('2018-02-18').getTime(),
-      y: 79
-    },
-    {
-      x: new Date('2018-02-19').getTime(),
-      y: 80
-    },
-    {
-      x: new Date('2018-02-20').getTime(),
-      y: 76
-    },
-    {
-      x: new Date('2018-02-21').getTime(),
-      y: 78
-    },
-    {
-      x: new Date('2018-02-22').getTime(),
-      y: 79
-    },
-    {
-      x: new Date('2018-02-23').getTime(),
-      y: 76
-    },
-  ]
-}]
+// TODO: auto update chart
+
+const chartSeries = [
+  {
+    name: "Ethereum",
+    data: [
+      {
+        x: new Date("2018-02-12").getTime(),
+        y: 76,
+      },
+      {
+        x: new Date("2018-02-13").getTime(),
+        y: 78,
+      },
+      {
+        x: new Date("2018-02-14").getTime(),
+        y: 79,
+      },
+      {
+        x: new Date("2018-02-15").getTime(),
+        y: 74,
+      },
+      {
+        x: new Date("2018-02-16").getTime(),
+        y: 76,
+      },
+      {
+        x: new Date("2018-02-17").getTime(),
+        y: 78,
+      },
+      {
+        x: new Date("2018-02-18").getTime(),
+        y: 79,
+      },
+      {
+        x: new Date("2018-02-19").getTime(),
+        y: 80,
+      },
+      {
+        x: new Date("2018-02-20").getTime(),
+        y: 76,
+      },
+      {
+        x: new Date("2018-02-21").getTime(),
+        y: 78,
+      },
+      {
+        x: new Date("2018-02-22").getTime(),
+        y: 79,
+      },
+      {
+        x: new Date("2018-02-23").getTime(),
+        y: 76,
+      },
+    ],
+  },
+];
 
 export default defineComponent({
   setup() {
     return {
       chartOptions,
-      chartSeries
-    }
-  }
-})
-
+      chartSeries,
+    };
+  },
+});
 </script>
