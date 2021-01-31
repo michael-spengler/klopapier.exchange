eed92885265ab7a261bfdbe15ff41a6a9ad2461a
Michael <michael.spengler@sap.com>
2021-02-01 00:19:00 +0100

    make UI more robust

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -37,7 +37,8 @@
             class="inline-flex items-center px-4 py-2 mx-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
             to="/depositEther"
           >
-            <span class="material-icons mr-2"> show_chart </span>
+          <!-- TODO: informed troy via telegram -> sometimes "show_chart" is shown instead of the icon itself -->
+            <!-- <span class="material-icons mr-2"> show_chart </span> -->
             Deposit Ether
           </router-link>
         </div>
@@ -47,7 +48,8 @@
             class="inline-flex items-center px-4 py-2 mx-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
             to="/wallstreetbetsCoins"
           >
-            <span class="material-icons mr-2"> account_balance </span>
+          <!-- TODO: informed troy via telegram -> sometimes "account_balance" is shown instead of the icon itself -->
+            <!-- <span class="material-icons mr-2"> account_balance </span> -->
             Get WallstreetbetCoins
           </router-link>
         </div>
