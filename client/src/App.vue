3be5e14f7b1471f9974a62e963ecdc19f084de79
Nevuor <fanzunkraut@gmail.com>
2021-03-19 15:52:51 +0100

    Adjustment of color scheme

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <nav class="bg-gray-800">
+    <nav>
       <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex items-center justify-between h-16">
           <div class="flex items-center">
@@ -201,104 +201,10 @@
   </div>
 </template>
 
-<script>
-import { computed, defineComponent, onMounted, ref } from "vue";
-import axios from "axios";
+<script src="./App.js">
 
-export default defineComponent({
-  setup() {
-    const menu = ref(false);
-
-    const walletAdress = ref(
-      localStorage.getItem("klopapier.exchange.account.walletAdress")
-    );
-    const etherBalance = ref(
-      localStorage.getItem("klopapier.exchange.account.etherBalance") || "0"
-    );
-
-    const walletAdressDisplay = computed(() => {
-      if (walletAdress.value) {
-        const adress = walletAdress.value;
-        return `${adress.substring(0, 4)}...${adress.substring(
-          adress.length - 4,
-          adress.length
-        )}`;
-      }
-      return "";
-    });
-
-    onMounted(() => {
-      window.ethereum.on("accountsChanged", (accounts) => {
-        if (accounts.length) {
-          localStorage.setItem("klopapier.exchange.account.walletAdress", accounts[0]);
-          walletAdress.value = accounts[0];
-          getBalanceInEthereum(walletAdress.value);
-        } else {
-          walletAdress.value = null;
-          localStorage.removeItem("klopapier.exchange.account.walletAdress");
-          localStorage.removeItem("klopapier.exchange.account.etherBalance");
-        }
-      });
-
-      if (walletAdress.value) {
-        getBalanceInEthereum();
-      }
-    });
-
-    const connect = async () => {
-      if (window.ethereum === undefined) {
-        alert(
-          "We recommend to install the brave.com browser to play the Klopapier Game."
-        );
-      } else {
-        const accounts = await window.ethereum.request({
-          method: "eth_requestAccounts",
-        });
-
-        localStorage.setItem("klopapier.exchange.account.walletAdress", accounts[0]);
-        walletAdress.value = accounts[0];
-        getBalanceInEthereum(walletAdress.value);
-      }
-    };
-
-    const getBalanceInEthereum = async (walletAdress) => {
-      const { data } = await axios.get(
-        `http://localhost:3001/getBalance/walletAddress/${walletAdress}`
-      );
-      etherBalance.value = data.balanceInEther;
-      localStorage.setItem(
-        "klopapier.exchange.account.etherBalance",
-        data.balanceInEther
-      );
-    };
-
-    const openGithub = () => {
-      window.open("https://github.com/michael-spengler/klopapier.exchange", "_blank");
-    };
-
-    const openTelegram = () => {
-      window.open("https://t.me/decentralizedfinancegroup", "_blank");
-    };
-
-    return {
-      connect,
-      walletAdress,
-      etherBalance,
-      walletAdressDisplay,
-      openGithub,
-      openTelegram,
-      menu,
-    };
-  },
-});
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<style scoped>
+  @import './App.css';
 </style>
