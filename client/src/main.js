b296b837b904a6a8d9d56fe60a2a2ea913bdc47a
Kev <Kevin.Krempels@gmail.com>
2021-03-31 12:18:22 +0200

    ffc vorlage

--- client/src/main.js
+++ client/src/main.js
@@ -4,4 +4,5 @@ import router from './router/index.ts'
 import VueApexCharts from "vue3-apexcharts";
 import './index.css'
 
-createApp(App).use(router).use(VueApexCharts).mount('#app')
+createApp(App).use(router).use(VueApexCharts).mount('#app');
+
--- client/src/views/educationInsight/educationInsight.vue
+++ client/src/views/educationInsight/educationInsight.vue
@@ -1,10 +1,31 @@
 
 <template>
-<v-app>
-<v-btn
-  elevation="2"
-></v-btn>
-</v-app>
+ <vs-card-group>
+ <vs-card type="5">
+    <template #title>
+      <h3>Pot with a plant</h3>
+    </template>
+    <template #img>
+      <img src="/foto5.png" alt="">
+    </template>
+    <template #text>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+      </p>
+    </template>
+    <template #interactions>
+      <vs-button danger icon>
+        <i class='bx bx-heart'></i>
+      </vs-button>
+      <vs-button class="btn-chat" shadow primary>
+        <i class='bx bx-chat' ></i>
+        <span class="span">
+          54
+        </span>
+      </vs-button>
+    </template>
+  </vs-card>
+  </vs-card-group>
 </template>
 
 <script src="./educationInsight.js"></script>
