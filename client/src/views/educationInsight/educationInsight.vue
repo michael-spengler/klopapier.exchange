2e458683125d047da2b14186759c49c3fd9840af
Kev <Kevin.Krempels@gmail.com>
2021-04-06 17:52:05 +0200

    Aktualisierung

--- client/src/views/educationInsight/educationInsight.vue
+++ client/src/views/educationInsight/educationInsight.vue
@@ -1,31 +1,81 @@
-
 <template>
- <vs-card-group>
- <vs-card type="5">
-    <template #title>
-      <h3>Pot with a plant</h3>
-    </template>
-    <template #img>
-      <img src="/foto5.png" alt="">
-    </template>
-    <template #text>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-      </p>
-    </template>
-    <template #interactions>
-      <vs-button danger icon>
-        <i class='bx bx-heart'></i>
-      </vs-button>
-      <vs-button class="btn-chat" shadow primary>
-        <i class='bx bx-chat' ></i>
-        <span class="span">
-          54
-        </span>
-      </vs-button>
-    </template>
-  </vs-card>
-  </vs-card-group>
+<body>
+
+<div class="bgimg-1">
+  <div class="caption">
+  <span class="border">Education Playground</span>
+  </div>
+</div>
+
+<div style="color:#Dbedf3;background-color:#283149
+;text-align:center;padding:50px 80px;text-align: justify;">
+  <h2 style="text-align:center; color:#Dbedf3;margin-top:2vh;letter-spacing:5px;">  Get the fundamental insights into Defi and Blockchain!</h2>
+</div>
+
+<div class="bgimg-2">
+  <div class="caption">
+ <!-- <span class="border" style="background-color:transparent;font-size:25px;color: #f7f7f7;">LESS HEIGHT</span>-->
+  </div>
+</div>
+
+<div style="position:relative;">
+  <div style="color:#ddd;background-color:#283149
+;text-align:center;padding:50px 80px;text-align: justify;">
+  <h3>Blockchain</h3>
+  <p class="contentData"> Blockchain is a specific type of database. It differs from a typical database in the way that it stores information. A Blockchain stores data in blocks that are chained together. New data will be stored in a fresh block. Once the block is created, the block will be chained onto the previous block. This enhances data storage in a chronological way. Different types can be stored on a blockchain but the most common use has been a ledger for transactions. The blocks are immutable, which means that the data entered is irreversible. Transactions are permanently recorded and viewable to anyone.
+ </p>
+  </div>
+</div>
+
+<div class="bgimg-3">
+  <div class="caption">
+  <!--<span class="border" style="background-color:transparent;font-size:25px;color: #f7f7f7;"></span>-->
+  </div>
+</div>
+
+<div style="position:relative;">
+  <div style="color:#ddd;background-color:#283149
+;text-align:center;padding:50px 80px;text-align: justify;">
+  <h3>Smart Contracts</h3>
+  <p class="contentData">
+A Smart Contract is a self-executing contract. Regarding an agreement between buyer and seller the contract terms are being written directly into the lines of code. It permits trusted transactions and agreements to be carried out among anonymous parties without the need of a central authority or legal system.
+Smart Contracts has been firstly defined by Nick Szabo in the year 1998 where he invented a virtual currency called ‘Bit Gold’. Therefore, he implemented a smart contract as computerized transaction protocols that execute terms of a contract.
+ </p>
+  </div>
+</div>
+
+<div class="bgimg-2">
+  <div class="caption">
+ <!-- <span class="border" style="background-color:transparent;font-size:25px;color: #f7f7f7;">LESS HEIGHT</span>-->
+  </div>
+</div>
+
+<div style="position:relative;">
+  <div style="color:#ddd;background-color:#283149
+;text-align:center;padding:50px 80px;text-align: justify;">
+  <h3>ERC 721 Tokens</h3>
+  <p class="contentData"> One can emulate rare, collectible items in real life with Ethereum tokens. Each of these tokens follows a standard in the Ethereum community called ERC721. Ethereum Request for Comments 721, or ERC721, can be defined as an Ethereum Improvement Proposal introduced by Dieter Shirley in late 2017. It’s a proposed standard allowing smart contracts to operate as tradeable tokens similar to ERC20. ERC721 Tokens are unique and non-fungible. (For more information about the propose see <a href="https://github.com/ethereum/EIPs/issues/721)"> here </a> )
+
+ </p>
+  </div>
+</div>
+
+<div class="bgimg-3">
+  <div class="caption">
+  <!--<span class="border" style="background-color:transparent;font-size:25px;color: #f7f7f7;"></span>-->
+  </div>
+</div>
+
+
+
+</body>
+
 </template>
 
+
 <script src="./educationInsight.js"></script>
+
+<style scoped>
+@import "./educationInsight.css";
+@import '../../../src/components/button/button.css';
+</style>
