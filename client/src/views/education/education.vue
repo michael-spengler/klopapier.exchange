59be246b09df5156fcb4f2aa8727df03182a5533
Kev <Kevin.Krempels@gmail.com>
2021-04-16 19:20:46 +0200

    FFCIntegration

--- client/src/views/education/education.vue
+++ client/src/views/education/education.vue
@@ -128,6 +128,73 @@
  <!-- <span class="border" style="background-color:transparent;font-size:25px;color: #f7f7f7;">LESS HEIGHT</span>-->
   </div>
 </div>
+<div class="background">
+      <div class="container">
+        <div class="screen">
+          <div class="screen-header">
+            <div class="screen-header-left">
+              <div class="screen-header-button close"></div>
+              <div class="screen-header-button maximize"></div>
+              <div class="screen-header-button minimize"></div>
+            </div>
+            <div class="screen-header-right">
+              <div class="screen-header-ellipsis"></div>
+              <div class="screen-header-ellipsis"></div>
+              <div class="screen-header-ellipsis"></div>
+            </div>
+          </div>
+          <div class="screen-body">
+            <div class="screen-body-item left">
+              <div class="app-title">
+                <span>Test Your</span>
+                <span>Knowledge</span>
+              </div>
+              <div class="app-contact">
+                No Pressure, this just need some time. Habt ihr in die Hosen
+                geschissen?
+              </div>
+            </div>
+            <div class="screen-body-item">
+              <div class="app-form">
+                <div class="app-form-group">
+                    Test your knowledge by downloading this <a download href="https://github.com/kevkre/klopapierFFC"> Fancy Flashcard</a>.
+       Use the amazing  Fancy Flashcard <a href="https://fancy-flashcard.github.io/ffc/#/"> Application </a> to test your Knowledge by using the Fancy Flashcard.
+   
+                </div>
+  
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="credits">
+          inspired by
+          <a
+            class="credits-link"
+            href="https://dribbble.com/shots/2666271-Contact"
+            target="_blank"
+          >
+            <svg class="dribbble" viewBox="0 0 200 200">
+              <g stroke="#ffffff" fill="none">
+                <circle cx="100" cy="100" r="90" stroke-width="20"></circle>
+                <path
+                  d="M62.737004,13.7923523 C105.08055,51.0454853 135.018754,126.906957 141.768278,182.963345"
+                  stroke-width="20"
+                ></path>
+                <path
+                  d="M10.3787186,87.7261455 C41.7092324,90.9577894 125.850356,86.5317271 163.474536,38.7920951"
+                  stroke-width="20"
+                ></path>
+                <path
+                  d="M41.3611549,163.928627 C62.9207607,117.659048 137.020642,86.7137169 189.041451,107.858103"
+                  stroke-width="20"
+                ></path>
+              </g>
+            </svg>
+            Gururaj
+          </a>
+        </div>
+      </div>
+    </div>
 
 <div style="position:relative;" >
   <div style="color:#ddd;background-color:#283149
@@ -326,12 +393,11 @@ Smart Contracts has been firstly defined by Nick Szabo in the year 1998 where he
 </div>
 
     <div class="mt-8">
-       Test your knowledge by downloading this <a download href="./DefiAndBlockchain.json"> Fancy Flashcard </a>.
-       Use the amazing  Fancy Flashcard <a href="https://fancy-flashcard.github.io/ffc/#/"> Application </a> to test your Knowledge by using the Fancy Flashcard.
-    </div>
+     </div>
 
-
+  
   </div>
+
   
 </template>
 
